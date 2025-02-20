0ae3ebdc49f9dc8d85948f5c905f1ef6657480cb
jeatzr <profesor.jeatienzar@iesazarquiel.es>
2025-02-20 12:43:17 +0100

    first commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import ThemeToggle from "~/components/ThemeToggle/ThemeToggle";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <ThemeToggle />
+    </div>
+  );
 }
